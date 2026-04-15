22b546625ea5f93c82091c2f275df7600d94fc88
DiogoCostadeGois <dioguitogois@hotmail.com>
2026-04-14 22:41:11 -0300

    140402026

--- src/db/database/database.module.ts
+++ src/db/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { DATABASE_URL, DRIZZLE } from './database.constants';
 import { drizzle } from 'drizzle-orm/node-mssql';
 import { Global } from '@nestjs/common';
+import { connect } from 'mssql';
+import type { config as MsSqlConfing } from 'mssql';
 import * as schema from '../schemas/index';
 
 @Global()
@@ -10,8 +12,22 @@ import * as schema from '../schemas/index';
     {
       provide: DRIZZLE,
       inject: [],
-      useFactory: () => {
-        return drizzle(DATABASE_URL, { schema: schema });
+      useFactory: async () => {
+        const dbConfig: MsSqlConfing = {
+          server: 'SRV-BD-1',
+          port: 1433,
+          user: 'alunos_des225',
+          password: '123',
+          database: 'des225_diogoc',
+          options: {
+            encrypt: false,
+            trustServerCertificate: true,
+          },
+        };
+
+        const pool = await connect(dbConfig);
+
+        return drizzle({ client: pool, schema: schema });
       },
     },
   ],
